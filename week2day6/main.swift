d7b21f4b7f25fc24bf345e95caef86a9617ddb9c
Kim Jungmin <87885175+wondermini@users.noreply.github.com>
2021-11-13 21:53:05 +0900

    for문 연습

--- week2day6/main.swift
+++ week2day6/main.swift
@@ -9,3 +9,26 @@ import Foundation
 
 print("Hello, World!")
 
+// M7.
+func printReverse(lines: Int) {
+    var temp = 0
+    for i in 1...lines {
+        var tempArr = [Int]()
+        for _ in 1...lines {
+            temp += 1
+            tempArr.append(temp)
+        }
+        if i % 2 == 0 {
+            tempArr = tempArr.reversed()
+        }
+        for j in tempArr {
+            if j<10{
+                print(" ", terminator: "")
+            }
+            print(j, terminator: " ")
+        }
+        print()
+    }
+}
+
+printReverse(lines: 4)
